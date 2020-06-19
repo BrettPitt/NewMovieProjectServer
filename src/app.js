0ee3151b074fab82fe06aa0bca26cfd9625124b4
BrettPitt <david.buergler89@gmail.com>
2020-06-19 22:44:00 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,58 @@
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017";
+const dbName = 'NewMovieWorld';
+const collectionName = 'favouritList';
+let db = undefined;
+let collection = undefined;
+
+/**
+ * Setup express middleware
+ */
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    next();
+});
+
+
+// Connect to database
+MongoClient.connect(url, {useUnifiedTopology: true}, function (err, connection) {
+    if (err) throw err;
+    db = connection.db(dbName);
+    collection = db.collection(collectionName);
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello dear User');
+});
+
+
+app.post('/NewMovieWorld', (req, res) => {
+    console.log('im posting into mongoDB right now');
+    const todo = req.body;
+    collection.insertOne(todo, function (err, result) {
+        if (err) throw err;
+        res.send({result: 'favorite inserted', todo: todo});
+    });
+});
+
+
+/**
+ * Start server
+ */
+app.listen(port, () => {
+    console.log(`Favorite app listening at http://localhost:${port}`);
+});
